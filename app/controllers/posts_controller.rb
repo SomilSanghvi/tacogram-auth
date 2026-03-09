f22bafae361f1cacd8e7b7aa696cf847d982ef10
SomilSanghvi <somil.sanghvi@kellogg.northwestern.edu>
2026-03-09 01:44:59 +0000

    did exercises

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,20 @@ class PostsController < ApplicationController
 
   def new
   end
-  
-  def create
-    @post = Post.new
-    @post["body"] = params["body"]
-    @post["image"] = params["image"]
-    # TODO: assign logged-in user as user that created the post
-    @post.save
-    redirect_to "/posts"
+
+
+def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @post = Post.new
+      @post["body"] = params["body"]
+      @post["image"] = params["image"]
+      @post["user_id"] = @user["id"]
+      @post.save
+    else
+      flash["notice"] = "Login first."
+    end
+      redirect_to "/posts"
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,26 @@ class SessionsController < ApplicationController
   def new
   end
   
-  def create
-    # TODO: authenticate user
-    flash["notice"] = "Nope."
+def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
 end
